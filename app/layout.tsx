989fd5eaef666d44c80c3bc387116b78b7817162
Kaleb <kalebzambrano@outlook.com>
2025-03-08 21:24:24 -0500

    Starting exercise

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css'
+import { monserrat } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${monserrat.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+import {Montserrat, Inter, Lusitana} from 'next/font/google'
+
+export const monserrat = Montserrat({subsets: ['latin'], weight: '400'})
+export const lusitana = Lusitana({subsets:['latin'], weight:'400'})
+export const inter = Inter({subsets:['latin'], weight: ['600', '800']})
